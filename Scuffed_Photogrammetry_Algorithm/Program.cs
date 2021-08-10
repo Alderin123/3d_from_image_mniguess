83105ec2c6b198f36ffe33c98bd742ec0eca583a
alderin123 <k.simon.soderstrom@gmail.com>
2021-08-10 17:51:40 +0200

    first commit

---
+++ Scuffed_Photogrammetry_Algorithm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Scuffed_Photogrammetry_Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
